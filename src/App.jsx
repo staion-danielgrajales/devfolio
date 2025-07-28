c0ab522ea300e61b36df2554b6cceb6d765f791b
danielgr19 <dani.grra19@gmail.com>
2025-07-28 11:33:35 -0500

    Add devfolio basic structure

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './assets/styles/fonts.css'
+import './assets/styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      <section className='panel-container'>
+        <div id='left-container'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Modi nulla ab similique dolorem consequuntur necessitatibus laborum,
+          ex minima sequi cumque praesentium ullam quis culpa accusantium magni! Mollitia facilis quos libero.
+        </div>
+
+        <div id='right-container'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Modi nulla ab similique dolorem consequuntur necessitatibus laborum,
+          ex minima sequi cumque praesentium ullam quis culpa accusantium magni! Mollitia facilis quos libero.
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
